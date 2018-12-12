7c80e0013ad221f563ae67cd5d94b646b5ee174f
mingz <305603665@qq.com>
2018-12-12 20:26:11 +0800

    ios

---
+++ src/ios/ISFyberAdapter.framework/Versions/A/Headers/ISFyberAdapter.h
@@ -0,0 +1,17 @@
+//
+//  ISFyberAdapter.h
+//  ISFyberAdapter
+//
+//  Created by Gili Ariel on 14/03/2018.
+//  Copyright © 2018 Gili Ariel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IronSource/ISBaseAdapter+Internal.h"
+
+@interface ISFyberAdapter : ISBaseAdapter
+
+
+
+
+@end
